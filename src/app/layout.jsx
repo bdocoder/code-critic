69847e2cc09af5612e8e76dc826d14d5ad9f846e
Abdullah Ahmad <bdocoder@protonmail.com>
2023-11-24 15:25:14 +0200

    feat: integrate next-themes for dark theme support

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,6 +7,7 @@ import Navbar from "./Navbar";
 import { getUserId } from "@/utils/server";
 import Sidebar from "./Sidebar";
 import DTLogger from "@/components/DTLogger";
+import NextThemeProvider from "./NextThemeProvider";
 
 export default function RootLayout({ children }) {
   const id = getUserId();
@@ -15,15 +16,17 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <DTLogger />
-        <Theme>
-          <div className="min-h-screen bg-accent-1 grid grid-cols-[240px_1fr] grid-rows-[52px_1fr]">
-            <Navbar />
-            {id && <Sidebar />}
-            <main className={`flex flex-col${id ? "" : " col-span-2"}`}>
-              {children}
-            </main>
-          </div>
-        </Theme>
+        <NextThemeProvider>
+          <Theme>
+            <div className="min-h-screen bg-accent-1 grid grid-cols-[240px_1fr] grid-rows-[52px_1fr]">
+              <Navbar />
+              {id && <Sidebar />}
+              <main className={`flex flex-col${id ? "" : " col-span-2"}`}>
+                {children}
+              </main>
+            </div>
+          </Theme>
+        </NextThemeProvider>
       </body>
     </html>
   );
